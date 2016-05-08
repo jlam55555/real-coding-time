45c7087d503c54676793b4cd0ab8e89c9311be06
Jonathan Lam <jlam55555@gmail.com>
2016-05-08 12:44:39 -0400

    Changed routing

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
 var app = require("express")();
-var server = require("http").createServer();
-var io = require("socket.io")(server);
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
 io.on("connection", function(socket) {
   console.log("connected");
   socket.on("disconnect", function() {
     console.log("disconnected");
   });
 });
-/*app.all("/", function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});*/
-app.get("/*", function(req, res) {
-  res.sendFile(__dirname + "/public/" + req.url);
-  console.log(__dirname + "/public/" + req.url);
-});
-server.listen(3000);
+app.use(require("express").static(__dirname + "/public"));
+http.listen(3000);
