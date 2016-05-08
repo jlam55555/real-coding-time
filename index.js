ad34ed1a95a6d0e6c65ee28b307a9cb50a5372e5
Jonathan Lam <jlam55555@gmail.com>
2016-05-08 11:50:45 -0400

    Fixed syntax error

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var server = require("http").createServer();
 var io = require("socket.io")(server);
-io.set("transports", ["websocket"]);
-io.set("origins", "jlam55555.github.com/realcodingtime/:*"
+//io.set("origins", "jlam55555.github.com/realcodingtime/:*");
 io.on("connection", function(socket) {
   console.log("connected");
   socket.on("disconnect", function() {
