04186f579f840d71cd2ee00cd138c7ccebfbe268
Jonathan Lam <jlam55555@gmail.com>
2016-05-08 12:01:35 -0400

    Added dummy files

--- index.js
+++ index.js
@@ -12,4 +12,7 @@ app.all("/", function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
 });
+app.get("/*", function(req, res) {
+  res.sendFile(__dirname + "/public/" + req.URL);
+});
 server.listen(3000);
