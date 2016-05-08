5d8caeb8d5afcec013c30fefffe77fb5be7e3fc9
Jonathan Lam <jlam55555@gmail.com>
2016-05-08 12:58:33 -0400

    Added code and refactored

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var http = require("http").createServer();
 var io = require("socket.io")(http);
-io.on("connection", function(socket) {
-  console.log("connected");
-  socket.on("disconnect", function() {
-    console.log("disconnected");
-  });
+
+var code = "<!DOCTYPE html>\n<html>\n  <head>\n    <title>Hello, World!</title>\n  </head>\n  <body>\n    <h1>Hello, World!</h1>\n  </body>\n</html>";
+io.on("connection", socket => {
+  socket.emit("code", code);
+  socket.on("disconnect", () => {});
 });
 http.listen(process.env.PORT || 5000);
