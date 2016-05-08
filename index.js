d32d8208d22e867ce4f7babe0d2cf59e5a1d376a
Jonathan Lam <jlam55555@gmail.com>
2016-05-08 11:55:06 -0400

    Trying new headers

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+var app = require("express")();
 var server = require("http").createServer();
 var io = require("socket.io")(server);
-//io.set("origins", "jlam55555.github.com/realcodingtime/:*");
 io.on("connection", function(socket) {
   console.log("connected");
   socket.on("disconnect", function() {
     console.log("disconnected");
   });
+});
+app.all("/", function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
 });
 server.listen(3000);
