8c929b90c00f8c281dc849d351a3e12166ed082b
Jonathan Lam <jlam55555@gmail.com>
2016-05-08 13:23:29 -0400

    Updating code software

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var io = require("socket.io")(http);
 var code = "<!DOCTYPE html>\n<html>\n  <head>\n    <title>Hello, World!</title>\n  </head>\n  <body>\n    <h1>Hello, World!</h1>\n  </body>\n</html>";
 io.on("connection", socket => {
   socket.emit("code", code);
+  socket.on("codeUpdate", newCode => socket.broadcast.emit("code", (code = newCode)) );
   socket.on("disconnect", () => {});
 });
 http.listen(process.env.PORT || 5000);
